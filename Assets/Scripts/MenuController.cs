7f7f996262b6e0d5668beedc2b9ce5a3b084f846
erre808 <ermiasgonday@gmail.com>
2024-09-27 17:10:44 +0200

    Played around woth the UI.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameLibrary()
     {
-        
+
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
 }
