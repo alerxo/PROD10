abbe2a3549de236fad3d40c4d7b5393ac94c8cdf
Aletsux <71812424+Aletsux@users.noreply.github.com>
2024-10-25 12:06:41 +0200

    Goal, restart game

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class Goal : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             audioSource.Play();
+            SceneManager.LoadScene("StartMenu");
         }
     }
 }
